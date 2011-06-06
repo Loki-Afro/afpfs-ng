893f20e42e2ab5f650f633c38a05afa26943f20f
Yuval Tal <yuvalt@boxee.tv>
2011-06-06 15:56:38 +0300

    fix install dir

--- configure_canmore.sh
+++ configure_canmore.sh
@@ -3,4 +3,4 @@
 export CXX=/opt/canmore/IntelCE/bin/i686-cm-linux-g++
 export CC=/opt/canmore/IntelCE/bin/i686-cm-linux-gcc
 
-./configure CPPFLAGS=-I/opt/canmore/local/include LDFLAGS=-L/opt/canmore/local/lib
+./configure CPPFLAGS=-I/opt/canmore/local/include LDFLAGS=-L/opt/canmore/local/lib --prefix=/opt/canmore/local
--- install_canmore.sh
+++ install_canmore.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-make && sudo make install prefix=/opt/canmore/local
+make && sudo make install
